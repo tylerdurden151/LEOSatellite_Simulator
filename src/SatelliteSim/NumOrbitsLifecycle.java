53c84a16a496b0a5228f8085caa1f4d366bfe4f5
OperationOrion <mclaughlin.mitch@yahoo.com>
2025-04-02 19:20:20 -0400

    Create NumOrbitsLifecycle.java
    created NumOrbitsLifecycle.java that calculates the number of orbits during satellite lifecycle by taking lifecycle seconds and dividing it by the orbital period

---
+++ src/SatelliteSim/NumOrbitsLifecycle.java
@@ -0,0 +1,36 @@
+package application;
+
+public class NumOrbitsLifecycle {
+    private static final double DEFAULT_LIFECYCLE_SECONDS = 5 * 365.25 * 24 * 60 * 60; // 5 years
+    private final Satellite satellite;
+    private final double lifecycleSeconds;
+
+    public NumOrbitsLifecycle(Satellite satellite) {
+        this.satellite = satellite;
+        this.lifecycleSeconds = DEFAULT_LIFECYCLE_SECONDS;
+    }
+
+    public NumOrbitsLifecycle(Satellite satellite, double lifecycleYears) {
+        if (lifecycleYears <= 0) {
+            throw new IllegalArgumentException("Lifecycle duration must be positive.");
+        }
+        this.satellite = satellite;
+        this.lifecycleSeconds = lifecycleYears * 365.25 * 24 * 60 * 60;
+    }
+
+    // Calculates number of orbits over entire lifecycle.
+  
+    public long calculateNumberOfOrbits() {
+        double period = OrbitalPeriod.calculatePeriod(satellite);
+        return (long) (lifecycleSeconds / period);
+    }
+
+    public double getLifecycleYears() {
+        return lifecycleSeconds / (365.25 * 24 * 60 * 60);
+    }
+    //function test for tyler B---------------------------------------------
+    public static void main(String[] args) {
+        Satellite testSat = new Satellite(500, 400_000, 0, 4);
+        System.out.println();
+    }
+}
