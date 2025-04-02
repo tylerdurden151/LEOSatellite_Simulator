780149ceaa68c87f2a66a9bb3ca0899e883d0ee4
tylerdurden151 <tylerdurden_151@yahoo.com>
2025-04-02 08:46:55 -0700

    Separated files into individual classes

--- src/SatelliteSim/Main.java
+++ src/SatelliteSim/Main.java
@@ -2,140 +2,25 @@
  * File name: Main.java
  * Author: Timothy Eckart
  * source: afsalashyana
- * Date: 26 Mar 2025
+ * Date: 2 Apr 2025
  * Purpose:
  *
  */
 
 //last edit done by Tim E 07:40 28 Mar 2025
-// Hello This is my own Branch
 
 package SatelliteSim;
 	
-import javafx.animation.AnimationTimer;
+
 import javafx.application.Application;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.scene.Camera;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Sphere;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private static final float WIDTH = 1400;
-    private static final float HEIGHT = 1000;
-
-    private double anchorX, anchorY;
-    private double anchorAngleX = 0;
-    private double anchorAngleY = 0;
-    private final DoubleProperty angleX = new SimpleDoubleProperty(0);
-    private final DoubleProperty angleY = new SimpleDoubleProperty(0);
-
-    private final Sphere sphere = new Sphere(150);
-
-    
+    @Override
     public void start(Stage primaryStage) {
-        Camera camera = new PerspectiveCamera(true);
-        camera.setNearClip(1);
-        camera.setFarClip(10000);
-        camera.translateZProperty().set(-1000);
-
-        Group world = new Group();
-        world.getChildren().add(prepareEarth());
-
-        Group root = new Group();
-        root.getChildren().add(world);
-        root.getChildren().add(prepareImageView());
-
-        Scene scene = new Scene(root, WIDTH, HEIGHT, true);
-        scene.setFill(Color.SILVER);
-        scene.setCamera(camera);
-
-        initMouseControl(world, scene, primaryStage);
-
-        primaryStage.setTitle("SatelliteSimulator");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        prepareAnimation();
-    }
-
-    private void prepareAnimation() {
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                sphere.rotateProperty().set(sphere.getRotate() + 0.2);
-            }
-        };
-        timer.start();
-    }
-
-    private Node prepareEarth() {
-        PhongMaterial earthMaterial = new PhongMaterial();
-        try {
-            // Use file: protocol for absolute paths
-            //String earthPath = "file:C:/PROJECTNAMEHERE/src/Images/earth-d.jpg";
-            //earthMaterial.setDiffuseMap(new Image(earthPath));
-            earthMaterial.setDiffuseMap(new Image(getClass().getResourceAsStream("/resources/earth/earth-d.jpg")));
-            earthMaterial.setSelfIlluminationMap(new Image(getClass().getResourceAsStream("/resources/earth/earth-l.jpg")));
-            earthMaterial.setSpecularMap(new Image(getClass().getResourceAsStream("/resources/earth/earth-s.jpg")));
-            earthMaterial.setBumpMap(new Image(getClass().getResourceAsStream("/resources/earth/earth-n.jpg")));
-        } catch (Exception e) {
-            System.err.println("Error loading Earth texture: " + e.getMessage());
-            earthMaterial.setDiffuseColor(Color.BLUE); // Fallback color
-        }
-
-        sphere.setRotationAxis(Rotate.Y_AXIS);
-        sphere.setMaterial(earthMaterial);
-        return sphere;
-    }
-
-    private ImageView prepareImageView() {
-        ImageView imageView = new ImageView();
-        try {
-            // Use file: protocol for absolute paths
-            String galaxyPath = "/resources/galaxy/galaxy.jpg";
-            Image image = new Image(galaxyPath);
-            imageView.setImage(image);
-            imageView.setPreserveRatio(true);
-            imageView.getTransforms().add(new Translate(-image.getWidth() / 2, -image.getHeight() / 2, 800));
-        } catch (Exception e) {
-            System.err.println("Error loading galaxy image: " + e.getMessage());
-        }
-        return imageView;
-    }
-
-    private void initMouseControl(Group group, Scene scene, Stage stage) {
-        Rotate xRotate;
-        Rotate yRotate;
-        group.getTransforms().addAll(
-                xRotate = new Rotate(0, Rotate.X_AXIS),
-                yRotate = new Rotate(0, Rotate.Y_AXIS)
-        );
-        xRotate.angleProperty().bind(angleX);
-        yRotate.angleProperty().bind(angleY);
-
-        scene.setOnMousePressed(event -> {
-            anchorX = event.getSceneX();
-            anchorY = event.getSceneY();
-            anchorAngleX = angleX.get();
-            anchorAngleY = angleY.get();
-        });
-
-        scene.setOnMouseDragged(event -> {
-            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
-            angleY.set(anchorAngleY + anchorX - event.getSceneX());
-        });
+        UserInterface ui = new UserInterface();
+        ui.build(primaryStage);
     }
 
     public static void main(String[] args) {
---
+++ src/SatelliteSim/Simulation.java
@@ -0,0 +1,88 @@
+package SatelliteSim;
+
+import javafx.animation.AnimationTimer;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.Camera;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.stage.Stage;
+
+import static SatelliteSim.Earth.prepareImageView;
+
+
+public class Simulation {
+
+    private static final float WIDTH = 1400;
+    private static final float HEIGHT = 1000;
+    private double anchorX, anchorY;
+    private double anchorAngleX = 0;
+    private double anchorAngleY = 0;
+    private final DoubleProperty angleX = new SimpleDoubleProperty(0);
+    private final DoubleProperty angleY = new SimpleDoubleProperty(0);
+    private final Sphere sphere = Earth.prepareEarth();
+
+    public void start(Stage primaryStage) {
+        Camera camera = new PerspectiveCamera(true);
+        camera.setNearClip(1);
+        camera.setFarClip(10000);
+        camera.translateZProperty().set(-1000);
+
+        Group world = new Group();
+        world.getChildren().add(sphere);
+
+        Group root = new Group();
+        root.getChildren().add(world);
+        root.getChildren().add(prepareImageView());
+
+        Scene scene = new Scene(root, WIDTH, HEIGHT, true);
+        scene.setFill(Color.SILVER);
+        scene.setCamera(camera);
+
+        initMouseControl(world, scene, primaryStage);
+
+        primaryStage.setTitle("SatelliteSimulator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        prepareAnimation();
+    }
+
+    private void initMouseControl(Group world, Scene scene, Stage primaryStage) {
+        Rotate xRotate;
+        Rotate yRotate;
+        world.getTransforms().addAll(
+                xRotate = new Rotate(0, Rotate.X_AXIS),
+                yRotate = new Rotate(0, Rotate.Y_AXIS)
+        );
+        xRotate.angleProperty().bind(angleX);
+        yRotate.angleProperty().bind(angleY);
+
+        scene.setOnMousePressed(event -> {
+            anchorX = event.getSceneX();
+            anchorY = event.getSceneY();
+            anchorAngleX = angleX.get();
+            anchorAngleY = angleY.get();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
+            angleY.set(anchorAngleY + anchorX - event.getSceneX());
+        });
+    }
+
+
+    private void prepareAnimation() {
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                sphere.rotateProperty().set(sphere.getRotate() + 0.2);
+            }
+        };
+        timer.start();
+    }
+}
