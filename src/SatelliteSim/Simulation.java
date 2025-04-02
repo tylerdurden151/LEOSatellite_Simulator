6be8e372c36aa08dc4210bf3e2b591ebc33e8046
tylerdurden151 <tylerdurden_151@yahoo.com>
2025-04-02 15:17:11 -0700

    Made Earth Private

--- src/SatelliteSim/Simulation.java
+++ src/SatelliteSim/Simulation.java
@@ -12,7 +12,6 @@ import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
-import static SatelliteSim.Earth.prepareImageView;
 
 
 public class Simulation {
@@ -24,7 +23,7 @@ public class Simulation {
     private double anchorAngleY = 0;
     private final DoubleProperty angleX = new SimpleDoubleProperty(0);
     private final DoubleProperty angleY = new SimpleDoubleProperty(0);
-    private final Sphere sphere = Earth.prepareEarth();
+    private final Sphere sphere = Earth.getSphere();
 
     public void start(Stage primaryStage) {
         Camera camera = new PerspectiveCamera(true);
@@ -37,7 +36,7 @@ public class Simulation {
 
         Group root = new Group();
         root.getChildren().add(world);
-        root.getChildren().add(prepareImageView());
+        root.getChildren().add(Earth.getImageView());
 
         Scene scene = new Scene(root, WIDTH, HEIGHT, true);
         scene.setFill(Color.SILVER);
