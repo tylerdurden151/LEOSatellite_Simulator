7ec5df176b8295947f726160604c352e4ef42a59
OperationOrion <mclaughlin.mitch@yahoo.com>
2025-04-02 20:35:51 -0400

    Update Simulation.java
    this now has changes for the orbit equatorial circle object, as well as calls an instance of the earth and satellite body objects

--- src/SatelliteSim/Simulation.java
+++ src/SatelliteSim/Simulation.java
@@ -1,4 +1,4 @@
-package SatelliteSim;
+package application;
 
 import javafx.animation.AnimationTimer;
 import javafx.beans.property.DoubleProperty;
@@ -8,14 +8,10 @@ import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
-
-
 public class Simulation {
-
     private static final float WIDTH = 1400;
     private static final float HEIGHT = 1000;
     private double anchorX, anchorY;
@@ -23,20 +19,26 @@ public class Simulation {
     private double anchorAngleY = 0;
     private final DoubleProperty angleX = new SimpleDoubleProperty(0);
     private final DoubleProperty angleY = new SimpleDoubleProperty(0);
-    private final Sphere sphere = Earth.getSphere();
+    private Earth earth; // Instance of Earth instead of static access
+    private Satellite satellite; // To pass to Earth
 
     public void start(Stage primaryStage) {
+        // Create a sample Satellite (replace with user input if desired)
+        satellite = new Satellite(500, 400_000, 0, 4); // 500 kg, 400 km, circular, 4 m^2
+        earth = new Earth(satellite); // Initialize Earth with Satellite
+
         Camera camera = new PerspectiveCamera(true);
         camera.setNearClip(1);
         camera.setFarClip(10000);
         camera.translateZProperty().set(-1000);
 
         Group world = new Group();
-        world.getChildren().add(sphere);
+        world.getChildren().add(earth.getSphere()); // Add Earth sphere
+        world.getChildren().add(earth.getOrbitCircle()); // Add orbit circle
 
         Group root = new Group();
         root.getChildren().add(world);
-        root.getChildren().add(Earth.getImageView());
+        root.getChildren().add(earth.getImageView()); // Use Earth's ImageView
 
         Scene scene = new Scene(root, WIDTH, HEIGHT, true);
         scene.setFill(Color.SILVER);
@@ -74,12 +76,12 @@ public class Simulation {
         });
     }
 
-
     private void prepareAnimation() {
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                sphere.rotateProperty().set(sphere.getRotate() + 0.2);
+                earth.getSphere().rotateProperty().set(earth.getSphere().getRotate() + 0.2);
+                // Orbit circle remains fixed (no rotation)
             }
         };
         timer.start();
