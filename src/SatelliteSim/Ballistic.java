3ffcf94e1a7309b1c84959083f2e4ab384d09fcc
OperationOrion <mclaughlin.mitch@yahoo.com>
2025-04-06 20:56:14 -0400

    Update Ballistic.java
    Newest edition as of 9pm EST 4/6/25

--- src/SatelliteSim/Ballistic.java
+++ src/SatelliteSim/Ballistic.java
@@ -2,14 +2,15 @@ package application;
 
 public class Ballistic {
     private static final double DRAG_COEFFICIENT = 2.2; // Typical for satellites
-
+   
     // return Ballistic coefficient in kg/m^2
     public static double calculateBallisticCoefficient(Satellite satellite) {
         return satellite.getMass() / (DRAG_COEFFICIENT * satellite.getArea());
     }
     //function test for tyler B----------------------
     public static void main(String[] args) {
-        Satellite testSat = new Satellite(500, 400_000, 0, 4);
-        System.out.println();
+    	Satellite testSat = new Satellite("Zeus", 500, 4, 400_000);
+        double coef = Ballistic.calculateBallisticCoefficient(testSat);
+        System.out.println("Ballistic coefficient: " + coef + " kg/m^2");
     }
 }
