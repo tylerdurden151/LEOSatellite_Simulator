034aa92784c448dcd104ff1971a3e60b780c92b5
OperationOrion <mclaughlin.mitch@yahoo.com>
2025-04-06 20:54:47 -0400

    Update Main.java
    Newest edition as of 9pm EST 4/6/25

--- src/SatelliteSim/Main.java
+++ src/SatelliteSim/Main.java
@@ -1,23 +1,12 @@
-/* Project name: CMSC495
- * File name: Main.java
- * Author: Timothy Eckart
- * source: afsalashyana
- * Date: 2 Apr 2025
- * Purpose:
- *
- */
-
-//last edit done by Tim E 07:40 28 Mar 2025
-
-package SatelliteSim;
+package application;
 	
+//last edit done by MITCH 9PM EST 4/6/25
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
     public void start(Stage primaryStage) {
         UserInterface ui = new UserInterface();
         ui.build(primaryStage);
